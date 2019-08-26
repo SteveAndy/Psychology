a229cec96796576c9f54c4854b213f3f7046fe73
loveempathy <39721333+loveempathy@users.noreply.github.com>
2019-08-26 21:39:17 +0800

    Update setinfo.js

--- PsychologyICE/setinfo.js
+++ PsychologyICE/setinfo.js
@@ -3,15 +3,14 @@ const md5 = require('utils/md5.min.js'),
     JgSecret = '02c0c4e49cd0ecbf4753876a', 
     RandomStr = 'E422A978DE37196588531CD0C42010B5',
     TimeStamp = (new Date()).getTime(),
-    RootUrl = 'https://nxxlzx.tpengyun.com/',
-    MiniProgramAppid = 'wx737c3211676c7ff5',//跳转小程序appid
-    //IM用户配置s
-    UserCode = 'e',
+    RootUrl = '服务器地址',
+    MiniProgramAppid = 'appid',//跳转小程序appid
+    //IM用户配置
+    UserCode = 'e',//前缀+user_id
     UserPwd = '123456';
 function signature() {
     return md5("appkey=" + JgAppkey
         + "&timestamp=" + TimeStamp
         + "&random_str=" + RandomStr + "&key=" + JgSecret)
 }
-//3995eddf8c8a8d0dac84030cdb831941
 module.exports = { JgAppkey,JgSecret,RandomStr,TimeStamp,signature,RootUrl,UserCode,UserPwd }
